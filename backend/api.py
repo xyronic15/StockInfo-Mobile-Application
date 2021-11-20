9bd784399c45420ae62bf5c4b2cf36e952a69426
Janahan10 <janahan.ravichandran@gmail.com>
2021-11-20 11:18:23 -0500

    Made small change to add main to remote

--- backend/api.py
+++ backend/api.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import backref
 from werkzeug.datastructures import T
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stocks-app.db'
 
