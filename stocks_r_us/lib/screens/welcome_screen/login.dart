6f1e404a5f8468e486f57980a10b69c3a61e6814
Janahan10 <janahan.ravichandran@gmail.com>
2021-11-22 11:45:38 -0500

    Login now works properly and brings user to home screen on successful login

--- stocks_r_us/lib/screens/welcome_screen/login.dart
+++ stocks_r_us/lib/screens/welcome_screen/login.dart
@@ -1,12 +1,19 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:stocks_r_us/ResponseBuilder.dart';
 
-Future<http.Response> userLogin(String username, String password) {
-  return http.post(Uri.parse("http://10.0.0.115:5000/login"),
+Future<ResponseBuilder> userLogin(String username, String password) async {
+  final response = await http.post(Uri.parse("http://10.0.0.115:5000/login"),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
       body: jsonEncode(
           <String, String>{"username": username, "password": password}));
+
+  if (response.statusCode == 200) {
+    return ResponseBuilder.fromJson(jsonDecode(response.body));
+  }
+
+  return ResponseBuilder(message: "Login Error", statusCode: 400);
 }
