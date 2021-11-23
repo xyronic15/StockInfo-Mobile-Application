1d7178fc12ee7f86ab13512e59267dfe0b3c1a8d
Janahan10 <janahan.ravichandran@gmail.com>
2021-11-22 20:38:58 -0500

    App now adds new favorites to the main list

--- stocks_r_us/lib/Stock.dart
+++ stocks_r_us/lib/Stock.dart
@@ -3,15 +3,16 @@ import 'package:flutter/rendering.dart';
 class Stock {
   final String name;
   final String ticker;
+  final int id;
 
-  Stock({required this.name, required this.ticker});
+  Stock({required this.name, required this.ticker, required this.id});
 
   factory Stock.fromJson(Map<dynamic, dynamic> json) {
-    return Stock(name: json["name"], ticker: json["ticker"]);
+    return Stock(name: json["name"], ticker: json["ticker"], id: json["id"]);
   }
 
   @override
   String toString() {
-    return "name: ${name}, ticker: ${ticker}";
+    return "name: ${name}, ticker: ${ticker}, id: ${id}";
   }
 }
---
+++ stocks_r_us/lib/screens/add_stock/getStock.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:stocks_r_us/ResponseBuilder.dart';
+import 'package:stocks_r_us/Stock.dart';
+import 'dart:convert';
+
+import 'package:stocks_r_us/UserId.dart';
+
+Future<List<dynamic>> getAllStocks() async {
+  final String stocksEndpoint = 'http://10.0.0.115:5000/list_all_stocks';
+
+  final stocksResponse = await http.get(Uri.parse("$stocksEndpoint"));
+
+  List<dynamic> stocksJson = json.decode(stocksResponse.body);
+
+  List<dynamic> stocks = [];
+
+  for (var i = 0; i < stocksJson.length; i++) {
+    stocks.add(Stock.fromJson(stocksJson[i]));
+  }
+
+  return stocks;
+}
+
+Future<ResponseBuilder> addFavourite(String username, int stockId) async {
+  final String getIdEndpoint = 'http://10.0.0.115:5000/get_user';
+  final idResponse =
+      await http.get(Uri.parse("$getIdEndpoint?username=$username"));
+
+  print(jsonDecode(idResponse.body));
+  UserId user = UserId.fromJson(jsonDecode(idResponse.body));
+
+  print("ID =============== ${user.id.toString()}");
+
+  final id = user.id;
+  print("ID ============> $id");
+  print("STOCK ID ==============> $stockId");
+
+  final String addEndpoint = 'http://10.0.0.115:5000/add_fav';
+  final response = await http.put(Uri.parse("http://10.0.0.115:5000/add_fav"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, int>{"userID": id, "stockID": stockId}));
+
+  if (response.statusCode == 200) {
+    return ResponseBuilder.fromJson(jsonDecode(response.body));
+  }
+
+  return ResponseBuilder(message: "Error adding new favorite", statusCode: 400);
+}
